ee53603261c699fcad55e5d52cf53a029c9ca52a
rond <rond.nunes@gmail.com>
2019-02-19 14:25:29 +0000

    enhancing parsing

---
+++ TODO.md
@@ -0,0 +1,1 @@
+* Check author's internal referencing system; ie you have 5 authors, 3 of them at the same institution, the affiliation won't be repeated, but instead referenced internally; fix this shit.
--- scielo.php
+++ scielo.php
@@ -5,20 +5,26 @@ error_reporting(E_ALL);
 $arquivo = simplexml_load_string(file_get_contents("artigo_2.xml"));
 // var_dump($arquivo->body->sec[0]->title);
 
-/* foreach  ($arquivo->body->sec as $sec)
-{
-	echo "<h1>{$sec->title}</h1>";
-	if(count($sec->p) > 0)
-	{
-		foreach($sec->p as $p)
-		{
-			echo "<p>{$p}</p>";
-		} 
-	} else
-	{
-		echo "<p>{$sec->p}</p>";
-	}
-}*/
+ echo "<p style='text-align: right'>{$arquivo->front->{'journal-meta'}->{'journal-id'}} (ISSN: {$arquivo->front->{'journal-meta'}->issn})</p><h2 style='text-align: center'>{$arquivo->front->{'article-meta'}->{'title-group'}->{'article-title'}[0]}</h2> <h3 style='font-style: italic; text-align: center'>{$arquivo->front->{'article-meta'}->{'title-group'}->{'article-title'}[1]}</h3><br /><br />";
 
-echo "<p style='text-align: right'>{$arquivo->front->{'journal-meta'}->{'journal-id'}} (ISSN: {$arquivo->front->{'journal-meta'}->issn})</p><h2 style='text-align: center'>{$arquivo->front->{'article-meta'}->{'title-group'}->{'article-title'}[0]}</h2> <h3 style='font-style: italic; text-align: center'>{$arquivo->front->{'article-meta'}->{'title-group'}->{'article-title'}[1]}</h3><br /><br />";
+$i = 0; //totally fucked, won't work because authors info are interally referenced
+foreach($arquivo->front->{'article-meta'}->{'contrib-group'}->contrib as $contrib){
+echo "<b>{$contrib->name->surname} {$contrib->name->{'given-names'}}</b> ({$arquivo->front->{'article-meta'}->{'contrib-group'}->aff[$i]->institution}, {$arquivo->front->{'article-meta'}->{'contrib-group'}->aff[$i]->country})<br />";
+$i++;
+}
+
+foreach  ($arquivo->body->sec as $sec)
+{
+        echo "<h1>{$sec->title}</h1>";
+        if(count($sec->p) > 0)
+        {
+                foreach($sec->p as $p)
+                {
+                        echo "<p>{$p}</p>";
+                }
+        } else
+        {
+                echo "<p>{$sec->p}</p>";
+        }
+} 
 ?>
