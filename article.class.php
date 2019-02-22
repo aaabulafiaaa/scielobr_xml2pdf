0c53066eb03ab23451efb49b660a125691024aeb
aaabulafiaaa <rond.nunes@gmail.com>
2019-02-22 10:17:22 -0300

    article class; no longer unbloated, sorry

---
+++ article.class.php
@@ -0,0 +1,27 @@
+<?php
+class article {
+	public $journal = array(
+		'title' =>,
+		'doi' =>,
+		'issn' =>,
+		'isbn' =>,
+		'publisher' =>,
+		'license' =>);
+	
+	public $issue = array(
+		'release' =>,
+		'number' =>,
+		'issue' =>,
+		'title' =>,
+		'pages' =>,);
+
+	public $article = array(
+		'abstract' => array(
+			'original' => array(),
+			'translation' => array()),
+		'authors' => array(),
+		'id' =>,
+		'attrib' =>,
+	);
+?>
+
