ed29fb35876c35dc3ff1c13a7830b01d00180a83
rond <rond.nunes@gmail.com>
2019-02-23 21:19:19 +0000

    more fields

--- article.class.php
+++ article.class.php
@@ -10,7 +10,7 @@ class article extends FPDF {
 	public $issue = array();
 	public $authors = array();
 	public $article = array();	
-	private $xml;
+	public $xml;
 
 	public function __construct($xml, $loadAll = TRUE) {
 		$this->xml = simplexml_load_string($xml, NULL, LIBXML_NOCDATA);
@@ -74,6 +74,12 @@ class article extends FPDF {
 		$this->article["metadata"] = array(
 			"doi" => (string) $this->xml->front->{'article-meta'}->{'article-id'}[0],
 			"title" =>  (string) $this->xml->front->{'article-meta'}->{'title-group'}->{'article-title'},
+			"pages" => array(
+				(int) $this->xml->front->{'article-meta'}->fpage,
+				(int) $this->xml->front->{'article-meta'}->lpage),
+			"funding" => array(
+				(string) $this->xml->front->{'article-meta'}->{'funding-group'}->{'award-group'}->{'funding-source'},
+				(string) $this->xml->front->{'article-meta'}->{'funding-group'}->{'award-group'}->{'award-id'}),
 			"history" => array(
 					"received" => (int) mktime(0, 0, 0, (int) $this->xml->front->{'article-meta'}->history->date[0]->month, (int) $this->xml->front->{'article-meta'}->history->date[0]->day, (int) $this->xml->front->{'article-meta'}->history->date[0]->year),
 					"accepted" => (int) mktime(0, 0, 0, (int) $this->xml->front->{'article-meta'}->history->date[1]->month, (int) $this->xml->front->{'article-meta'}->history->date[1]->day, (int) $this->xml->front->{'article-meta'}->history->date[1]->year)
@@ -95,6 +101,42 @@ class article extends FPDF {
 			$bi +=1;
 			}
 		}
+
+		if(count($this->xml->front->{'article-meta'}->{'trans-abstract'}) >= 0) {
+		// $this->article["abstract"][] = $this->xml->front->{'article-meta'}->abstract[0];
+		$bi_t=0;
+			foreach($this->xml->front->{'article-meta'}->{'trans-abstract'} as $abstract)
+			{
+			$this->article["trans_abstract"][$bi_t]["title"] = (string) $abstract->title;
+				foreach($abstract->sec as $sec)
+				{
+				$this->article["trans_abstract"][] = array("title" => (string) $sec->title,
+				"text" => (string) $sec->p);
+				}
+			$bi_t++;
+			}
+		}
+
+			$i=0;
+		foreach($this->xml->body->sec as $sect)
+		{
+			if(count($sect->sec) > 1)
+			{
+				$this->article["content"][$i]["title"] = (string) $sect->title;
+				foreach ($sect->sec as $subsect)
+				{
+					$this->article["content"][$i]["sub"][] = array(
+						"title" => (string) $subsect->title,
+						"text" => (string) $subsect->p);
+				}
+			} else
+			{
+					$this->article["content"][] = array(
+                                        	"title" => (string) $sect->title,
+                                        	"text" => (string) $sect->p);
+			}
+			$i++;
+		}
 	}
 }
 
@@ -102,7 +144,7 @@ $artigo = new article(file_get_contents("artigo2.xml"));
 echo "<pre>";
 var_dump(json_decode(json_encode($artigo)));
 echo "<h2> ARTICLE META</h2>";
-var_dump( $artigo->xml->front->{'journal-meta'});
+var_dump($artigo->xml->front->{'article-meta'}->{'trans-abstract'});
 echo "</pre>";
 ?>
 
